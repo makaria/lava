5cccbcd03b962c383c95bb4050ec5e5c1bc0b262
makaria <makarites@gmail.com>
2019-03-24 17:02:13 +0800

    calculated with worker.

--- src/components/lava.vue
+++ src/components/lava.vue
@@ -1,74 +1,69 @@
 <template>
-  <div class="layout">
-    <div class="layout-header">
+  <div class="lava">
+    <h2 class="lava-title fire">Lava Foundation</h2>
+    <div class="actions" :style="{ width: `${totemSize * col}px` }">
+      <label for="edit" class="label edit" :class="editing ? 'bg-fire' : 'bg-water'">
+        {{ editing ? 'Editing' : 'Playing' }}
+        <input
+          id="edit"
+          type="checkbox"
+          class="input"
+          v-model="editing"
+        >
+      </label>
+      <template v-if="editing">
+        <label for="row" class="label row">
+          Row:
+          <input id="row" type="number" min="1" max="1000" class="input" v-model="row">
+        </label>
+        <label for="col" class="label col">
+          Col:
+          <input id="col" type="number" min="1" max="1000" class="input" v-model="col">
+        </label>
+      </template>
+      <button v-if="!editing" class="hint bg-water" @click="hint">{{ hintText }}</button>
+      <button v-else class="reset bg-water" @click="reset">Reset</button>
     </div>
-    <div class="layout-content">
-      <div class="layout-view">
-        <div class="layout-padding">
-          <div class="lava">
-            <!-- <h2>Lava Foundation</h2> -->
-            <div class="actions">
-              <label for="edit" class="label edit" :class="editing ? 'bg-fire' : 'bg-water'">
-                {{ editing ? 'Editing' : 'Playing' }}
-                <input id="edit" type="checkbox" class="input" v-model="editing">
-              </label>
-              <template v-if="editing">
-                <label for="row" class="label row">
-                  Row:
-                  <input id="row" type="number" min="1" max="1000" class="input" v-model="row">
-                </label>
-                <label for="col" class="label col">
-                  Col:
-                  <input id="col" type="number" min="1" max="1000" class="input" v-model="col">
-                </label>
-              </template>
-              <button v-if="!editing" class="hint bg-water" @click="hint">{{ hintText }}</button>
-              <button v-else class="reset bg-water" @click="reset">Reset</button>
-              <!-- <button class="test" @click="test">Test</button> -->
-            </div>
-            <div class="azeroth">
-              <icons></icons>
-              <svg  ref="board"
-                    class="maelstrom"
-                    :width="totemSize * col"
-                    :height="totemSize * row"
-                    @contextmenu.prevent
-                    >
-                    <totem  v-for="(totem, index) in totems"
-                            :key="index"
-                            :totem="totem"
-                            :size="totemSize"
-                            @toggle="toggleTotem"
-                            :editing="editing"></totem>
-              </svg>
-            </div>
-            <div class="audio">
-              <audio ref="water" src="./static/water.wav"></audio>
-              <audio ref="fire" src="./static/fire.wav"></audio>
-              <audio ref="success" src="./static/success.wav"></audio>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div class="azeroth">
+      <icons></icons>
+      <svg
+        ref="board"
+        class="maelstrom"
+        :width="totemSize * col"
+        :height="totemSize * row"
+        @contextmenu.prevent
+      >
+        <totem
+          v-for="(totem, index) in totems"
+          :key="index"
+          :totem="totem"
+          :size="totemSize"
+          @toggle="toggleTotem"
+          :editing="editing"
+        ></totem>
+      </svg>
     </div>
-    <div class="layout-footer"></div>
+    <div class="audio">
+      <audio ref="water" src="./static/water.wav"></audio>
+      <audio ref="fire" src="./static/fire.wav"></audio>
+      <audio ref="success" src="./static/success.wav"></audio>
+    </div>
   </div>
 </template>
 
 <script>
-import icons from './icons'
-import totem from './totem'
-// import DIE from '../modules/exhaustion'
-import Matrix from '../modules/matrix'
-import { getNeighbors } from '../modules/util'
+import icons from "./icons";
+import totem from "./totem";
+import { getNeighbors } from "../modules/util";
+import Worker from "../modules/matrix.worker.js";
 
 export default {
-  name: 'lava',
+  name: "lava",
   components: {
     totem,
     icons
   },
-  data () {
+  data() {
     return {
       row: 5,
       col: 5,
@@ -79,86 +74,93 @@ export default {
       editing: false,
       showHint: false,
       result: [],
-      prevState: '',
+      prevState: "",
       prevHintIndex: 0
-    }
+    };
   },
   computed: {
-    totemSize () {
-      return Math.min(this.width / this.colNum, this.height / this.rowNum)
+    totemSize() {
+      return Math.min(this.width / this.colNum, this.height / this.rowNum);
     },
-    status () {
-      let status = []
+    status() {
+      let status = [];
       for (let key in this.totems) {
-        let totem = this.totems[key]
-        let index = totem.row * this.colNum + totem.col
-        status[index] = totem.off ? 0 : 1
+        let totem = this.totems[key];
+        let index = totem.row * this.colNum + totem.col;
+        status[index] = totem.off ? 0 : 1;
       }
-      return status
+      return status;
     },
-    state () {
+    state() {
       return this.status.reduce((a, b) => {
-        return a + b
-      }, '')
+        return a + b;
+      }, "");
     },
-    weight () {
+    weight() {
       if (this.status.length > 0) {
         return this.status.reduce((a, b) => {
-          return a + b
-        }, 0)
+          return a + b;
+        }, 0);
       } else {
-        return 42
+        return 42;
       }
     },
-    colNum () {
-      return ~~this.col
+    colNum() {
+      return ~~this.col;
     },
-    rowNum () {
-      return ~~this.row
+    rowNum() {
+      return ~~this.row;
     },
-    hintText () {
-      let amount = this.result.length
-      return `Hint${amount > 0 ? ' ' + ((this.prevHintIndex % amount) + 1) + '/' + amount : ''}`
+    hintText() {
+      let amount = this.result.length;
+      return `Hint${
+        amount > 0
+          ? " " + ((this.prevHintIndex % amount) + 1) + "/" + amount
+          : ""
+      }`;
     }
   },
-  created () {
-    window.onresize = this.resize
+  created() {
+    window.addEventListener("resize", this.resize);
+    const worker = new Worker();
+    worker.addEventListener("message", this.handleWorkerCallback);
+    this.worker = worker;
   },
   watch: {
-    col () {
-      this.createTotems()
+    col() {
+      this.createTotems();
     },
-    row () {
-      this.createTotems()
+    row() {
+      this.createTotems();
     },
-    weight () {
+    weight() {
       if (!this.editing && this.weight === 0) {
-        this.done()
+        this.done();
       }
     },
-    editing () {
-      this.isPlayable()
+    editing() {
+      this.isPlayable();
     }
   },
-  mounted () {
-    this.resize()
-    this.init()
-    // this.audio = this.$refs.audio
+  mounted() {
+    this.$nextTick(() => {
+      this.resize();
+      this.init();
+    });
   },
   methods: {
-    init: function () {
-      this.createTotems()
+    init() {
+      this.createTotems();
     },
-    resize: function () {
-      // let size = Math.min(window.innerHeight, window.innerWidth) * 0.85
-      this.width = window.innerWidth * 0.85
-      this.height = window.innerHeight * 0.8
+    resize() {
+      this.width = window.innerWidth - 16 * 2;
+      this.height = window.innerHeight - 34 - 16 * 3 - 28 - 16;
     },
-    createTotems: function () {
-      let totems = {}
+    createTotems() {
+      let totems = {};
       for (let i = 0; i < this.rowNum; i++) {
         for (let j = 0; j < this.colNum; j++) {
-          let index = i * this.colNum + j
+          let index = i * this.colNum + j;
           totems[index] = {
             row: i,
             col: j,
@@ -166,144 +168,181 @@ export default {
             clicked: false,
             hint: false,
             neighbors: getNeighbors(index, this.colNum, this.rowNum)
-          }
+          };
         }
       }
-      this.totems = totems
+      this.totems = totems;
     },
-    reset: function () {
+    reset() {
       for (let key in this.totems) {
-        let totem = this.totems[key]
+        let totem = this.totems[key];
         if (totem.off) {
-          totem.off = false
+          totem.off = false;
         }
       }
     },
-    toggleTotem: function (totem) {
-      this.audioPlay(totem.off)
-      let index = totem.row * this.colNum + totem.col
-      let patch = {}
+    toggleTotem(totem) {
+      this.audioPlay(totem.off);
+      let index = totem.row * this.colNum + totem.col;
+      let patch = {};
       if (!this.editing) {
-        let neighbors = this.totems[index].neighbors
+        let neighbors = this.totems[index].neighbors;
         for (let neighbor of neighbors) {
           patch[neighbor] = Object.assign({}, this.totems[neighbor], {
             off: !this.totems[neighbor].off
-          })
+          });
         }
       } else {
         patch[index] = Object.assign({}, this.totems[index], {
           off: !this.totems[index].off
-        })
+        });
       }
       patch[index] = Object.assign({}, patch[index], {
         clicked: !this.totems[index].clicked
-      })
-      this.totems = Object.assign({}, this.totems, patch)
+      });
+      this.totems = Object.assign({}, this.totems, patch);
     },
-    done: function () {
-      this.$refs.success.play()
+    done() {
+      this.$refs.success.play();
       setTimeout(() => {
-        this.nextBlock()
-      }, 500)
+        this.result = [];
+        this.nextBlock();
+        this.clearHint();
+      }, 500);
     },
-    hint: function () {
-      let hints
+    hint() {
+      let hints;
       if (this.prevState === this.state) {
-        this.prevHintIndex = this.prevHintIndex + 1
-        let index = this.prevHintIndex % (this.result.length)
-        hints = this.result[index]
+        this.prevHintIndex = this.prevHintIndex + 1;
+        let index = this.prevHintIndex % this.result.length;
+        hints = this.result[index];
+        this.updateHint(hints);
       } else {
-        this.prevState = this.state
-        this.prevHintIndex = 0
-        console.time('die')
-        // let result = DIE(this.state, this.colNum, this.rowNum)
-        let result = Matrix.magic(this.state, this.rowNum, this.colNum)
-        console.timeEnd('die')
-        result = this.sortByWeight(result)
-        this.result = result
-        hints = result[0]
-      }
-      if (hints) {
-        this.updateHint(hints)
-      } else {
-        console.error('No solution!')
+        this.prevState = this.state;
+        this.prevHintIndex = 0;
+        this.worker.postMessage({
+          state: this.state,
+          rowNum: this.rowNum,
+          colNum: this.colNum,
+          type: "hint"
+        });
       }
     },
-    updateHint: function (hints) {
-      let state = {}
+    handleHint(result) {
+      result = this.sortByWeight(result);
+      this.result = result;
+      const hints = result[0];
+      if (hints) {
+        this.updateHint(hints);
+      } else {
+        console.error("No solution!");
+      }
+    },
+    handlePlay(result) {
+      if (result.length === 0) {
+        alert("No solutions!");
+        this.editing = true;
+      }
+      if (this.prevState !== this.state) {
+        this.clearHint();
+      }
+    },
+    handleWorkerCallback(event) {
+      const { result, type } = event.data;
+      const handler = type === "play" ? this.handlePlay : this.handleHint;
+      handler(result);
+    },
+    updateHint(hints) {
+      let state = {};
       Array.from(hints).forEach((hint, index) => {
         state[index] = Object.assign({}, this.totems[index], {
-          hint: hint === '1',
+          hint: hint === "1",
           clicked: false
-        })
-      })
-      this.totems = Object.assign({}, this.totems, state)
+        });
+      });
+      this.totems = Object.assign({}, this.totems, state);
     },
-    clearHint: function () {
+    clearHint() {
       for (let key in this.totems) {
-        this.totems[key].hint = false
-        this.totems[key].clicked = false
+        this.totems[key].hint = false;
+        this.totems[key].clicked = false;
       }
-      this.result = []
+      this.result = [];
     },
-    sortByWeight: function (result) {
+    sortByWeight(result) {
       return result.sort((a, b) => {
-        let wa = a.replace(/0/g, '')
-        let wb = b.replace(/0/g, '')
-        return wa.length < wb.length
-      })
+        let wa = a.replace(/0/g, "");
+        let wb = b.replace(/0/g, "");
+        return wa.length < wb.length;
+      });
     },
-    nextBlock: function () {
-      this.row = ~~this.row + 1
-      this.col = ~~this.col + 1
+    nextBlock() {
+      this.row = ~~this.row + 1;
+      this.col = ~~this.col + 1;
     },
-    isPlayable: function () {
-      if (!this.editing) {
-        let result = Matrix.magic(this.state, this.rowNum, this.colNum)
-        if (result.length === 0) {
-          alert('No solutions!')
-          this.editing = true
-        }
-        if (this.prevState !== this.state) {
-          this.clearHint()
-        }
-      } else {
-        // this.result = []
-      }
+    isPlayable() {
+      if (this.editing) return;
+      this.worker.postMessage({
+        state: this.state,
+        rowNum: this.rowNum,
+        colNum: this.colNum,
+        type: "play"
+      });
     },
-    audioPlay: function (off) {
+    audioPlay(off) {
       if (off) {
-        let fire = this.$refs.fire
-        fire.currentTime = 0
-        fire.play()
+        let fire = this.$refs.fire;
+        fire.currentTime = 0;
+        fire.play();
       } else {
-        let water = this.$refs.water
-        water.currentTime = 0
-        water.play()
+        let water = this.$refs.water;
+        water.currentTime = 0;
+        water.play();
       }
     }
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.resize);
   }
-}
+};
 </script>
 
 <style scoped>
-.layout-padding {
-  padding: 1rem 1.5rem;
+.lava {
+}
+.lava-title {
+  margin: 16px 0;
+}
+
+.bg-fire {
+  background-color: #f44336;
+}
+
+.bg-water {
+  background-color: #2196f3;
+}
+
+.fire {
+  color: #f44336;
+}
+
+.water {
+  color: #2196f3;
 }
 
 .maelstrom {
-  /* background-color: #9e9e9e; */
   user-select: none;
 }
 
 .actions {
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
-  /* max-width: 60vw; */
-  margin: 1rem 20vw;
   user-select: none;
+  margin: auto;
+  margin-bottom: 16px;
+  min-width: 300px;
 }
 
 .row,
@@ -316,18 +355,17 @@ export default {
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
-  /* margin: 0 2vw; */
   cursor: pointer;
-  /* max-width: 5rem; */
+  margin-right: 16px;
+  width: 80px;
+  border-radius: 34px;
 }
 
 .edit {
-  padding: .5rem 1rem;
-  color: white;
+  padding: 0.5rem 0;
   font-size: 1rem;
   border: 0;
-  /* min-width: 5rem; */
-  /* background-color: #2196f3; */
+  color: black;
 }
 
 .edit > .input {
@@ -336,16 +374,19 @@ export default {
 
 .input {
   max-width: 2rem;
-  margin-left: .5rem;
+  margin-left: 0.5rem;
   text-align: center;
 }
 
-.hint, .reset {
-  /* min-width: 5rem; */
-  /* max-width: 3rem; */
-  color: white;
+.hint,
+.reset {
   border: 0;
-  padding: .5rem 1rem;
+  padding: 0.5rem 1rem;
   font-size: 1rem;
+  cursor: pointer;
+  color: black;
+  margin-left: 16px;
+  min-width: 80px;
+  border-radius: 34px;
 }
 </style>
--- src/components/totem.vue
+++ src/components/totem.vue
@@ -1,76 +1,72 @@
 <template>
   <g class="totem" :transform="transform" @click="toggle">
-    <rect class="totem"
-          :width="size"
-          :height="size"
-          :x="0"
-          :y="0">
-    </rect>
-    <svg class="icon" :class="iconClass" :width="size * scale" :height="size * scale" :x="size * offset" :y="size * offset">
+    <rect class="totem" :width="size" :height="size" :x="0" :y="0"></rect>
+    <svg
+      class="icon"
+      :class="iconClass"
+      :width="size * scale"
+      :height="size * scale"
+      :x="size * offset"
+      :y="size * offset"
+    >
       <use :xlink:href="icon"></use>
     </svg>
-    <!-- <image  :x="x"
-            :y="y"
-            :width="width"
-            :height="height"
-            :href="out"
-            /> -->
   </g>
 </template>
 
 <script>
 export default {
-  name: 'totem',
+  name: "totem",
   props: {
     totem: {},
     size: 0,
     editing: false
   },
-  data () {
-    return {
-      // clicked: false
-    }
+  data() {
+    return {};
   },
   computed: {
-    x () {
-      return this.totem.col * this.size
+    x() {
+      return this.totem.col * this.size;
     },
-    y () {
-      return this.totem.row * this.size
+    y() {
+      return this.totem.row * this.size;
     },
-    out () {
-      return this.totem.off ? 'water' : 'fire'
-      // let out = this.totem.off ? 'water' : 'fire'
-      // return this.hint ? out + ' ' + this.hint : out
+    out() {
+      return this.totem.off ? "water" : "fire";
     },
-    icon () {
-      return this.totem.off ? '#icon-water' : '#icon-fire'
+    icon() {
+      return this.totem.off ? "#icon-water" : "#icon-fire";
     },
-    transform () {
-      return `translate(${this.x}, ${this.y})`
+    transform() {
+      return `translate(${this.x}, ${this.y})`;
     },
-    scale () {
-      return this.totem.off ? 0.8 * 0.9 : 0.8
+    scale() {
+      return this.totem.off ? 0.8 * 0.9 : 0.8;
     },
-    offset () {
-      return (1 - this.scale) / 2
+    offset() {
+      return (1 - this.scale) / 2;
     },
-    hint () {
-      return (((this.totem.hint && !this.clicked) || (this.totem.hint === false && this.clicked)) && !this.editing) ? 'hint' : ''
+    hint() {
+      return ((this.totem.hint && !this.clicked) ||
+        (this.totem.hint === false && this.clicked)) &&
+        !this.editing
+        ? "hint"
+        : "";
     },
-    clicked () {
-      return this.totem.clicked
+    clicked() {
+      return this.totem.clicked;
     },
-    iconClass () {
-      return this.out + ' ' + this.hint
+    iconClass() {
+      return this.out + " " + this.hint;
     }
   },
   methods: {
-    toggle: function () {
-      this.$emit('toggle', this.totem)
+    toggle() {
+      this.$emit("toggle", this.totem);
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -96,11 +92,6 @@ export default {
 
 .icon {
   display: inline-block;
-  /* width: 1em;
-  height: 1em; */
-  /* stroke-width: 0; */
-  /* stroke: currentColor; */
-  /* fill: currentColor; */
 }
 </style>
 
